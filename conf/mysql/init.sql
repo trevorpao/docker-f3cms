edeab414a70be50c666e8819ee394932e943d830
Trevor Pao <shuaib25@gmail.com>
2018-08-16 08:47:46 +0800

    gp: remove useless modules update init.sql

--- conf/mysql/init.sql
+++ conf/mysql/init.sql
@@ -1,9 +1,7 @@
 
 --
--- 資料庫： `target_db` f3cms
--- f3cms sample db
+-- 資料庫： `target_db`
 --
-
 CREATE DATABASE IF NOT EXISTS `target_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `target_db`;
 
@@ -15,11 +13,20 @@ USE `target_db`;
 
 CREATE TABLE `sessions` (
   `session_id` varchar(255) NOT NULL,
-  `data` text DEFAULT NULL,
+  `data` text CHARACTER SET utf8mb4 DEFAULT NULL,
   `ip` varchar(45) DEFAULT NULL,
   `agent` varchar(300) DEFAULT NULL,
   `stamp` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 資料表的匯出資料 `sessions`
+--
+
+INSERT INTO `sessions` (`session_id`, `data`, `ip`, `agent`, `stamp`) VALUES
+('d9c07cabb1d7377b5be84ca249e03400', 'csrf|s:27:\"1ugxbqojkkcp0.2y3zxd4uj144w\";cs|a:3:{s:4:\"name\";s:5:\"admin\";s:2:\"id\";s:1:\"1\";s:9:\"has_login\";i:1;}', '172.18.0.1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36 FirePHP/0.7.4', 1534379896);
+
+-- --------------------------------------------------------
 
 --
 -- 資料表結構 `tbl_adv`
@@ -35,8 +42,8 @@ CREATE TABLE `tbl_adv` (
   `theme` varchar(10) DEFAULT NULL,
   `start_date` timestamp NULL DEFAULT NULL,
   `end_date` timestamp NULL DEFAULT NULL,
-  `uri` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `cover` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `uri` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `cover` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `background` varchar(255) NOT NULL,
   `last_ts` timestamp NOT NULL DEFAULT current_timestamp(),
   `last_user` int(11) NOT NULL,
@@ -81,6 +88,13 @@ CREATE TABLE `tbl_author` (
   `insert_user` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- 資料表的匯出資料 `tbl_author`
+--
+
+INSERT INTO `tbl_author` (`id`, `status`, `slug`, `online_date`, `cover`, `last_ts`, `last_user`, `insert_ts`, `insert_user`) VALUES
+(1, 'Enabled', 'author1', NULL, '/upload/img/2018/08/5a83d2fed624e53.png', '2018-08-16 00:31:16', 1, '2018-08-14 04:07:21', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -92,13 +106,21 @@ CREATE TABLE `tbl_author_lang` (
   `lang` varchar(5) NOT NULL DEFAULT 'tw',
   `parent_id` int(11) NOT NULL,
   `title` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `subtitle` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `slogan` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
   `content` text CHARACTER SET utf8mb4 DEFAULT NULL,
   `last_ts` timestamp NULL DEFAULT current_timestamp(),
   `last_user` int(11) DEFAULT NULL,
   `insert_ts` timestamp NULL DEFAULT NULL,
   `insert_user` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- 資料表的匯出資料 `tbl_author_lang`
+--
+
+INSERT INTO `tbl_author_lang` (`id`, `lang`, `parent_id`, `title`, `slogan`, `content`, `last_ts`, `last_user`, `insert_ts`, `insert_user`) VALUES
+(1, 'tw', 1, '作者1+1', '我也很無奈', '<p>夷</p>', '2018-08-16 00:31:16', 1, '2018-08-14 04:07:21', 1),
+(2, 'en', 1, 'author1', 'orz', '<p>en</p>', '2018-08-16 00:31:16', 1, '2018-08-14 04:07:21', 1);
 
 -- --------------------------------------------------------
 
@@ -110,6 +132,13 @@ CREATE TABLE `tbl_author_tag` (
   `author_id` int(11) NOT NULL,
   `tag_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 資料表的匯出資料 `tbl_author_tag`
+--
+
+INSERT INTO `tbl_author_tag` (`author_id`, `tag_id`) VALUES
+(1, 2);
 
 -- --------------------------------------------------------
 
@@ -157,12 +186,12 @@ CREATE TABLE `tbl_contact` (
   `id` int(11) NOT NULL,
   `status` enum('New','Process','Done') NOT NULL DEFAULT 'New',
   `type` varchar(50) DEFAULT NULL,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `phone` varchar(50) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `message` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `message` text CHARACTER SET utf8mb4 NOT NULL,
   `other` text DEFAULT NULL,
-  `response` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `response` text CHARACTER SET utf8mb4 NOT NULL,
   `last_ts` timestamp NOT NULL DEFAULT current_timestamp(),
   `last_user` int(11) NOT NULL,
   `insert_user` int(11) NOT NULL,
@@ -223,6 +252,13 @@ CREATE TABLE `tbl_media` (
   `insert_user` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- 資料表的匯出資料 `tbl_media`
+--
+
+INSERT INTO `tbl_media` (`id`, `status`, `slug`, `title`, `pic`, `info`, `last_ts`, `last_user`, `insert_ts`, `insert_user`) VALUES
+(1, 'Enabled', 'about', 'Hero Cover for AboutUs', '/upload/img/2018/08/9efef5748a38fb5.jpg', '', '2018-08-16 00:36:44', 1, '2018-08-14 06:27:20', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -258,7 +294,7 @@ CREATE TABLE `tbl_menu` (
   `id` int(11) NOT NULL,
   `status` enum('Enabled','Disabled') NOT NULL DEFAULT 'Disabled',
   `parent_id` int(11) DEFAULT 0,
-  `uri` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `uri` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `type` varchar(30) NOT NULL,
   `sorter` int(11) NOT NULL DEFAULT 0,
   `cover` varchar(150) DEFAULT NULL,
@@ -321,18 +357,13 @@ INSERT INTO `tbl_option` (`id`, `status`, `loader`, `group`, `name`, `content`, 
 (1, 'Enabled', 'Demand', 'page', 'title', 'F3CMS DEMO', '2017-12-28 21:49:32', 1, '2015-12-29 14:43:32', 1),
 (2, 'Enabled', 'Demand', 'page', 'keyword', 'key1,key2,key3', '2017-12-29 09:44:23', 1, '2015-12-29 14:44:11', 1),
 (4, 'Enabled', 'Demand', 'page', 'img', 'demo.png', '2017-12-28 21:45:11', 1, '2015-12-29 14:46:44', 1),
-(5, 'Enabled', 'Demand', 'social', 'facebook_page', 'https://www.facebook.com/', '2015-12-29 18:35:46', 1, '2015-12-29 18:35:46', 1),
+(5, 'Enabled', 'Preload', 'social', 'facebook_page', 'https://www.facebook.com/', '2015-12-29 18:35:46', 1, '2015-12-29 18:35:46', 1),
 (8, 'Enabled', 'Preload', 'default', 'contact_mail', 'sense.info.co@gmail.com', '2016-02-10 06:58:13', 1, '2016-02-02 10:08:41', 1),
-(12, 'Enabled', 'Demand', 'page', 'ga', '', '2017-03-27 02:52:28', 1, '2016-05-04 07:51:12', 1),
-(17, 'Enabled', 'Demand', 'page', 'pagetest', '', '2017-12-28 23:24:26', 1, '2017-12-28 23:24:26', 1),
-(18, 'Enabled', 'Demand', '這是G', '這是N', '這是C', '2017-12-29 09:24:48', 1, '2017-12-29 09:24:48', 1),
-(19, 'Enabled', 'Demand', 'Group01', 'soso', 'C1,C2,C3', '2017-12-29 09:26:51', 1, '2017-12-29 09:26:51', 1),
-(20, 'Enabled', 'Demand', 'Group02', 'soso2', 'C1,C2,C5', '2017-12-29 09:27:02', 1, '2017-12-29 09:27:02', 1),
-(21, 'Enabled', 'Demand', 'Group03', 'soso3', 'C1,C2,C7', '2017-12-29 09:27:11', 1, '2017-12-29 09:27:11', 1),
-(22, 'Enabled', 'Demand', 'Group04', 'soso3', 'C1,C2,C9', '2017-12-29 09:27:20', 1, '2017-12-29 09:27:20', 1),
-(23, 'Enabled', 'Demand', 'Group05', 'soso4', 'C1,C2,C10', '2017-12-29 09:28:17', 1, '2017-12-29 09:28:17', 1),
-(24, 'Enabled', 'Demand', 'Group06', 'soso5', 'C1,C2,C11', '2017-12-29 09:28:28', 1, '2017-12-29 09:28:28', 1),
-(25, 'Enabled', 'Demand', 'G1', 'N1', 'C1', '2017-12-29 09:32:02', 1, '2017-12-29 09:32:02', 1);
+(12, 'Enabled', 'Demand', 'page', 'ga', '<script></script>', '2018-08-09 02:58:41', 1, '2016-05-04 07:51:12', 1),
+(26, 'Enabled', 'Preload', 'default', 'contact_phone', '+ 33 9 07 45 12 65', '2016-02-10 06:58:13', 1, '2016-02-02 10:08:41', 1),
+(27, 'Enabled', 'Preload', 'default', 'contact_address', '42 rue Moulbert 75016 Paris', '2016-02-10 06:58:13', 1, '2016-02-02 10:08:41', 1),
+(28, 'Enabled', 'Preload', 'social', 'linkedin_page', 'https://www.linkedin.com/', '2015-12-29 18:35:46', 1, '2015-12-29 18:35:46', 1),
+(29, 'Enabled', 'Preload', 'social', 'twitter_page', 'https://twitter.com/', '2015-12-29 18:35:46', 1, '2015-12-29 18:35:46', 1);
 
 -- --------------------------------------------------------
 
@@ -356,8 +387,9 @@ CREATE TABLE `tbl_post` (
 --
 
 INSERT INTO `tbl_post` (`id`, `status`, `slug`, `cover`, `last_ts`, `last_user`, `insert_ts`, `insert_user`) VALUES
-(3, 'Enabled', '/about', '', '2017-04-01 15:55:14', 1, '2017-01-17 18:07:53', 1),
-(5, 'Enabled', '/contact', '', '2017-04-01 04:22:37', 1, '2017-03-26 02:22:21', 1);
+(3, 'Enabled', 'about', '/upload/img/2018/08/a39b5421afe3bdc.jpg', '2018-08-15 02:00:56', 1, '2017-01-17 18:07:53', 1),
+(5, 'Enabled', 'contact', '', '2018-08-16 00:18:56', 1, '2017-03-26 02:22:21', 1),
+(6, 'Enabled', 'privacy', '', '2018-08-16 00:21:01', 1, '2018-08-16 00:21:01', 1);
 
 -- --------------------------------------------------------
 
@@ -376,6 +408,20 @@ CREATE TABLE `tbl_post_lang` (
   `insert_ts` timestamp NULL DEFAULT NULL,
   `insert_user` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 資料表的匯出資料 `tbl_post_lang`
+--
+
+INSERT INTO `tbl_post_lang` (`id`, `lang`, `parent_id`, `title`, `content`, `last_ts`, `last_user`, `insert_ts`, `insert_user`) VALUES
+(1, 'tw', 3, '關於我們', '<p>異中經開十學海積為市人飛求難總！國結色好天動來特：起事感。導值遊該一我最市。不酒請了筆動。向就面飛。不老期前，想樣長氣是明到；是於效平，已不而海場同選野。識多所小、子來車路素分，人條這館東程我支新表國試你一育：那學傳苦裡做她保於接親要下樹書用因，必是樓外成足則、為區健之也輕設公局東術；預改人賽；的怎士樹生高痛畫形服年是到參西以單史！她市沒主她相任裡政家童們；年從來立氣司然城再一教險常為：同為商過停苦文環不從，過不賽條如面頭藝的語軍內成時的、生濟處狀過可公苦羅上長家人木人法車品術照長獲其叫……轉氣語；費麼下！好生水他次說們也這子場作這自意康得放形現實力配所，查持小任處人比看嚴系參；就語一引家公法有講氣回。</p><h3>Incididunt ut labore</h3><p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus iaculis magna sagittis elit sagittis, at hendrerit lorem venenatis. Morbi accumsan iaculis blandit. Cras ultrices hendrerit nisl.</p><h3>Ut enim ad minim veniam</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luctus at sem quis varius. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus iaculis magna sagittis elit sagittis, at hendrerit lorem venenatis. Morbi accumsan iaculis blandit. Cras ultrices hendrerit nisl.</p><h3>Phasellus iaculis magna</h3><p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>', '2018-08-15 02:00:56', 1, '2018-08-07 00:55:57', 1),
+(2, 'en', 3, 'About Us', '<p>Duis center aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><h3>Incididunt ut labore</h3><p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus iaculis magna sagittis elit sagittis, at hendrerit lorem venenatis. Morbi accumsan iaculis blandit. Cras ultrices hendrerit nisl.</p><h3>Ut enim ad minim veniam</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luctus at sem quis varius. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus iaculis magna sagittis elit sagittis, at hendrerit lorem venenatis. Morbi accumsan iaculis blandit. Cras ultrices hendrerit nisl.</p><h3>Phasellus iaculis magna</h3><p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>', '2018-08-15 02:00:56', 1, '2018-08-07 00:55:57', 1),
+(3, 'tw', 5, '聯絡我們~', '<p>中文 ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>', '2018-08-16 00:18:56', 1, '2018-08-07 00:55:57', 1),
+(4, 'en', 5, 'Contact us~', '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>', '2018-08-16 00:18:56', 1, '2018-08-07 00:55:57', 1),
+(5, 'tw', 6, '隱私權政策', '<p>本《隱私權政策》旨在協助您瞭解本站收集的資訊類型以及收集這些資訊的原因，也說明了您可以如何更新、管理、匯出與刪除資訊。</p>', '2018-08-16 00:21:01', 1, '2018-08-16 00:21:01', 1),
+(6, 'en', 6, '', '', '2018-08-16 00:21:01', 1, '2018-08-16 00:21:01', 1),
+(7, 'tw', 7, 'tmp', '', '2018-08-16 00:22:43', 1, '2018-08-16 00:22:43', 1),
+(8, 'en', 7, '', '', '2018-08-16 00:22:43', 1, '2018-08-16 00:22:43', 1);
 
 -- --------------------------------------------------------
 
@@ -552,6 +598,13 @@ CREATE TABLE `tbl_tag` (
   `insert_user` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- 資料表的匯出資料 `tbl_tag`
+--
+
+INSERT INTO `tbl_tag` (`id`, `status`, `parent_id`, `counter`, `slug`, `last_ts`, `last_user`, `insert_ts`, `insert_user`) VALUES
+(2, 'Enabled', 0, NULL, 'tag1', '2018-08-09 03:05:10', 1, '2018-08-01 02:30:03', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -570,6 +623,16 @@ CREATE TABLE `tbl_tag_lang` (
   `insert_ts` timestamp NULL DEFAULT NULL,
   `insert_user` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- 資料表的匯出資料 `tbl_tag_lang`
+--
+
+INSERT INTO `tbl_tag_lang` (`id`, `lang`, `parent_id`, `title`, `alias`, `info`, `last_ts`, `last_user`, `insert_ts`, `insert_user`) VALUES
+(1, 'tw', 1, 'tag1', NULL, NULL, '2018-08-01 02:05:49', NULL, NULL, NULL),
+(2, 'en', 1, 'tag1', NULL, NULL, '2018-08-01 02:05:49', NULL, NULL, NULL),
+(3, 'tw', 2, '標籤1', '別名1', NULL, '2018-08-09 03:05:10', 1, '2018-08-01 02:30:03', 1),
+(4, 'en', 2, 'tag1', 'alias2', NULL, '2018-08-09 03:05:10', 1, '2018-08-01 02:30:03', 1);
 
 -- --------------------------------------------------------
 
@@ -796,13 +859,13 @@ ALTER TABLE `tbl_adv_lang`
 -- 使用資料表 AUTO_INCREMENT `tbl_author`
 --
 ALTER TABLE `tbl_author`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- 使用資料表 AUTO_INCREMENT `tbl_author_lang`
 --
 ALTER TABLE `tbl_author_lang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- 使用資料表 AUTO_INCREMENT `tbl_contact`
@@ -826,7 +889,7 @@ ALTER TABLE `tbl_dictionary_lang`
 -- 使用資料表 AUTO_INCREMENT `tbl_media`
 --
 ALTER TABLE `tbl_media`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- 使用資料表 AUTO_INCREMENT `tbl_menu`
@@ -844,19 +907,19 @@ ALTER TABLE `tbl_menu_lang`
 -- 使用資料表 AUTO_INCREMENT `tbl_option`
 --
 ALTER TABLE `tbl_option`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- 使用資料表 AUTO_INCREMENT `tbl_post`
 --
 ALTER TABLE `tbl_post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- 使用資料表 AUTO_INCREMENT `tbl_post_lang`
 --
 ALTER TABLE `tbl_post_lang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- 使用資料表 AUTO_INCREMENT `tbl_press`
@@ -880,11 +943,11 @@ ALTER TABLE `tbl_staff`
 -- 使用資料表 AUTO_INCREMENT `tbl_tag`
 --
 ALTER TABLE `tbl_tag`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- 使用資料表 AUTO_INCREMENT `tbl_tag_lang`
 --
 ALTER TABLE `tbl_tag_lang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
